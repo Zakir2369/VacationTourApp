d5223e880a8263f09bc17cfb459b5eb3e157a333
zakir <Zakir.mia2369@gmail.com>
2023-01-01 00:35:26 +0600

    zakir

--- app/src/main/java/com/rajendra/sophorapplication/model/Hotel.java
+++ app/src/main/java/com/rajendra/sophorapplication/model/Hotel.java
@@ -4,15 +4,16 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class Hotel implements Serializable {
-    String hotelName, hotelLocation, hotelReview, hotelDetails;
-    String hotelWifi, HotelAc, hotelParking, hotelDining;
-    Date checkIn, checkOut;
-    int roomCount;
-    double hotelPrice;
-    String hotelImage, hotelImage1, hotelImage2, hotelImage3;
+    public String hotelName, hotelLocation, hotelReview, hotelDetails;
+    public String hotelPhone, hotelEmail;
+    public Date checkIn, checkOut;
+    public int roomCount;
+    public double hotelPrice;
+    public String hotelImage, hotelImage1, hotelImage2, hotelImage3;
+    public Boolean wifi=false, ac=false, restaurant=false, parking=false;
 
 
-    Hotel() {
+    public Hotel() {
 
     }
 
